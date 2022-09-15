f5f93d543793a3396c93d601ad4aee9225733e5c
Budaházi Bence <budahazi.bence@petrik.hu>
2022-09-15 11:23:20 +0200

    Két pont távolsága Síknegyed

--- src/hu/petrik/pontoop/Main.java
+++ src/hu/petrik/pontoop/Main.java
@@ -31,8 +31,16 @@ public class Main {
             }
         }
 
-        System.out.printf("A legtávolabbi pont az origótól a(z) %d. pont, koordinátái: %s, origótól mért távolsága: %.3f"
+        System.out.printf("A legtávolabbi pont az origótól a(z) %d. pont, " +
+                        "koordinátái: %s, origótól mért távolsága: %.3f\n"
                 , (legtavolabbiPontIndex + 1), pontok[legtavolabbiPontIndex],
                 pontok[legtavolabbiPontIndex].getOrigotolMertTavolsag());
+
+        System.out.printf("Az egyik pont: %s, A másik pont: %s, A két pont távolsága: %.3f\n",
+                p3, p4, p3.getTavolsag(p4));
+
+        System.out.printf("A %s pont elhelyezkedése: %s\n", p3, p3.getSiknegyedSzoveges());
+        System.out.printf("A %s pont elhelyezkedése: %s\n", p2, p2.getSiknegyedSzoveges());
+        System.out.printf("A %s pont elhelyezkedése: %s\n", p4, p4.getSiknegyedSzoveges());
     }
 }
--- src/hu/petrik/pontoop/pont/Pont.java
+++ src/hu/petrik/pontoop/pont/Pont.java
@@ -26,6 +26,58 @@ public class Pont {
     public double getOrigotolMertTavolsag() {
         // négyzetgyök - SQuareRooT || hatványozás - POWer
         return Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2));
+        //return this.getTavolsag(new Pont());
+    }
+
+    public double getTavolsag(Pont masikPont) {
+        return Math.sqrt(Math.pow(this.x - masikPont.x, 2) + Math.pow(this.y - masikPont.y, 2));
+    }
+
+    public int getSiknegyed() {
+        int siknegyed = 0;
+        if (this.x < 0 && this.y > 0) {
+            siknegyed = 1;
+        } else if (this.x < 0 && this.y < 0){
+            siknegyed = 2;
+        } else if (this.x > 0 && this.y < 0){
+            siknegyed = 3;
+        } else if (this.x > 0 && this.y > 0){
+            siknegyed = 4;
+        } else if (this.x == 0 && this.y != 0) {
+            siknegyed = 5;
+        } else if (this.x != 0 && this.y == 0) {
+            siknegyed = 6;
+        }
+        return siknegyed;
+    }
+
+    public String getSiknegyedSzoveges(){
+        int siknegyed = this.getSiknegyed();
+        String siknegyedSzoveges = "";
+        switch (siknegyed){
+            case 0:
+                siknegyedSzoveges = "origo";
+                break;
+            case 1:
+                siknegyedSzoveges = "bal felső síknegyed";
+                break;
+            case 2:
+                siknegyedSzoveges = "bal alsó síknegyed";
+                break;
+            case 3:
+                siknegyedSzoveges = "jobb alsó síknegyed";
+                break;
+            case 4:
+                siknegyedSzoveges = "jobb felső síknegyed";
+                break;
+            case 5:
+                siknegyedSzoveges = "függőleges tengely";
+                break;
+            case 6:
+                siknegyedSzoveges = "vízszintes tengely";
+                break;
+        }
+        return siknegyedSzoveges;
     }
 
     @Override
