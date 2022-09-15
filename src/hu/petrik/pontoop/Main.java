9b12efdd4cbd346552d763b17586491e8f27a75d
Budaházi Bence <budahazi.bence@petrik.hu>
2022-09-15 09:36:18 +0200

    100 db véletlen -10,10 koordinátákkal létrehozott pont közül az origótól legtávolabbi

--- src/hu/petrik/pontoop/Main.java
+++ src/hu/petrik/pontoop/Main.java
@@ -5,8 +5,8 @@ import hu.petrik.pontoop.pont.Pont;
 public class Main {
     public static void main(String[] args) {
         Pont p1 = new Pont();
-        Pont p2 = new Pont(0,0);
-        Pont p3 = new Pont(3,4);
+        Pont p2 = new Pont(0, 0);
+        Pont p3 = new Pont(3, 4);
         Pont p4 = new Pont(100);
 
 //        System.out.println(p1);
@@ -14,13 +14,25 @@ public class Main {
 //        System.out.println(p3);
 //        System.out.println(p4);
 
-        Pont[] pontok = new Pont[10];
+        Pont[] pontok = new Pont[100];
         for (int i = 0; i < pontok.length; i++) {
-            pontok[i] = new Pont(100);
+            pontok[i] = new Pont(10);
         }
 
-        for (Pont p: pontok) {
+        for (Pont p : pontok) {
             System.out.println(p);
         }
+
+        //Origótól legtávolabb eső pont - maximum kiválasztás
+        int legtavolabbiPontIndex = 0;
+        for (int i = 1; i < pontok.length; i++) {
+            if (pontok[legtavolabbiPontIndex].getOrigotolMertTavolsag() < pontok[i].getOrigotolMertTavolsag()) {
+                legtavolabbiPontIndex = i;
+            }
+        }
+
+        System.out.printf("A legtávolabbi pont az origótól a(z) %d. pont, koordinátái: %s, origótól mért távolsága: %.3f"
+                , (legtavolabbiPontIndex + 1), pontok[legtavolabbiPontIndex],
+                pontok[legtavolabbiPontIndex].getOrigotolMertTavolsag());
     }
 }
