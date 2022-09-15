b25ffd57ac2d4cee122aa3ea116bcbee5536997d
Budaházi Bence <budahazi.bence@petrik.hu>
2022-09-15 09:11:20 +0200

    Pont osztály - Véletlen pont létrehozó konstruktor

--- src/hu/petrik/pontoop/pont/Pont.java
+++ src/hu/petrik/pontoop/pont/Pont.java
@@ -5,13 +5,22 @@ public class Pont {
     private int y;
 
     public Pont() {
-        x = 0;
-        y = 0;
+        this.x = 0;
+        this.y = 0;
     }
 
     public Pont(int x, int y) {
         this.x = x;
         this.y = y;
+    }
+
+    public Pont(int n) {
+        this.x = koordinataGeneralasa(n);
+        this.y = koordinataGeneralasa(n);
+    }
+
+    private int koordinataGeneralasa(int n) {
+        return (int) (Math.random() * ((2 * n) + 1)) - n;
     }
 
     @Override
